9f3e84725fc21c368b1b9770eead7fb8f448d077
Robert Terry <roberteterry@icloud.com>
2018-11-04 15:23:05 -0500

    Prior to going different direction on SettingsVC

--- Final_Project/Final_Project/SettingsVC.swift
+++ Final_Project/Final_Project/SettingsVC.swift
@@ -7,6 +7,7 @@
 //
 // Sources - Enums for datasource - https://github.com/ianhirschfeld/EnumTableViewTutorial/blob/master/EnumTableViewTutorial/ViewController.swift
 // Sources - quick refresher for making dynamic table views - https://swiftludus.org/how-to-create-dynamic-lists-of-items-with-uitableview/
+// Source - counting occurances in array - https://stackoverflow.com/questions/30545518/how-to-count-occurrences-of-an-element-in-a-swift-array
 
 import UIKit
 
@@ -15,20 +16,31 @@ class SettingsVC: UITableViewController {
     
     @IBOutlet weak var tableViewSettings: UITableView!
     
+    enum TableSection: Int {
+        case playerInfo = 0, boardSettings, total
+    }
+    
     let healthyFoods = ["Apple", "Orange", "Pear", "Grapefruit", "Potato", "Tomato", "Leek", "Tangerine"]
     
+    let settingsItems = ["Player Names": TableSection.playerInfo, "Colors": .playerInfo, "Board Size": .boardSettings]
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return TableSection.total.rawValue
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return healthyFoods.count
+        return settingsItems.filter{$0.value.rawValue == section}.count
+//        return healthyFoods.count
+        
+        
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "settingsItemCell", for: indexPath)
-        cell.textLabel?.text = healthyFoods[indexPath.row]
+//        cell.textLabel?.text = settingsItems[indexPath.row]
+        let items = Array(settingsItems.filter { $0.value.rawValue == indexPath.row }.keys)
+        cell.textLabel?.text = items[indexPath.row]
         return cell
     }
     
@@ -36,6 +48,6 @@ class SettingsVC: UITableViewController {
     // Use this to segue to the setting view...
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(healthyFoods[indexPath.row])
+//        print(setting[indexPath.row])
     }
 }
