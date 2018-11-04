3aa27d109e05c8eb000c28fc8b6dbc9b73a7b919
Robert Terry <roberteterry@icloud.com>
2018-11-04 13:50:08 -0500

    Working on table view for settings

---
+++ Final_Project/Final_Project/SettingsVC.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsVC.swift
+//  Final_Project
+//
+//  Created by Robert Terry on 11/4/18.
+//  Copyright © 2018 Robert Terry. All rights reserved.
+//
+// Sources - Enums for datasource - https://github.com/ianhirschfeld/EnumTableViewTutorial/blob/master/EnumTableViewTutorial/ViewController.swift
+
+import UIKit
+
+class SettingsVC: UITableViewController {
+    
+    @IBOutlet weak var tableViewSettings: SettingsVC!
+    
+  
+    
+    
+}
