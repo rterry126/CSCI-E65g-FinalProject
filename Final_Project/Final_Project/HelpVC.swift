78abc9cf0679e78545d58cbe90c324545a4a9ffa
Robert Terry <roberteterry@icloud.com>
2018-11-04 07:40:22 -0500

    Added HelpVC

---
+++ Final_Project/Final_Project/HelpVC.swift
@@ -0,0 +1,15 @@
+//
+//  HelpVC.swift
+//  Final_Project
+//
+//  Created by Robert Terry on 11/4/18.
+//  Copyright © 2018 Robert Terry. All rights reserved.
+//
+
+import UIKit
+import WebKit  // WKWebView
+
+class HelpVC: UIViewController {
+    
+    
+}
