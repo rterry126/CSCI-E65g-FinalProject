c26081c2be82a9a612ff9e17e0ec2c91ef212a54
Robert Terry <roberteterry@icloud.com>
2018-11-05 18:14:49 -0500

    Started DetailNamesVC

---
+++ Final_Project/Final_Project/DetailNamesVC.swift
@@ -0,0 +1,22 @@
+//
+//  DetailNamesVC.swift
+//  Final_Project
+//
+//  Created by Robert Terry on 11/5/18.
+//  Copyright © 2018 Robert Terry. All rights reserved.
+//
+
+import UIKit
+
+class DetailNamesVC: UITableViewController {
+    
+    override func viewDidLoad() {
+        print("Detail names VC loading")
+        
+        tableView.tableFooterView = UIView()
+
+    }
+    
+    
+    
+}
