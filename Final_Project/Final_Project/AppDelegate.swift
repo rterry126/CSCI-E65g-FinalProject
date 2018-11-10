c6665adc6ac1230ec54931c14e80762cf4991049
Robert Terry <roberteterry@icloud.com>
2018-11-09 23:29:41 -0500

    Added Firstore pods and sample code to AppDelegate

--- Final_Project/Final_Project/AppDelegate.swift
+++ Final_Project/Final_Project/AppDelegate.swift
@@ -8,6 +8,7 @@
 // Source - sharing state (i.e. models) between VC - https://code.tutsplus.com/tutorials/the-right-way-to-share-state-between-swift-view-controllers--cms-28474
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -27,6 +28,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        else {
 //            fatalError("Could not initialize GamePrefModel")
 //        }
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
+        
+        // Add a new document with a generated ID
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "first": "Ada",
+            "last": "Lovelace",
+            "born": 1815
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+        
+        // Add a second document with a generated ID.
+        ref = db.collection("users").addDocument(data: [
+            "first": "Alan",
+            "middle": "Mathison",
+            "last": "Turing",
+            "born": 1912
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+        
+        db.collection("users").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+        
         return true
     }
 
