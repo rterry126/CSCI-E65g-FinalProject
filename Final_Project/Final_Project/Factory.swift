897f33854f4d2c769e427aefdb8fdd11dc129de5
Robert Terry <roberteterry@icloud.com>
2018-12-18 06:47:52 -0500

    More rebasing cleanup

--- Final_Project/Final_Project/Factory.swift
+++ Final_Project/Final_Project/Factory.swift
@@ -1,6 +1,6 @@
 //
 //  Factory.swift
-//  
+//
 //
 //  Created by Robert Terry on 11/12/18.
 //
@@ -10,16 +10,16 @@ import UIKit        // Alert creaton function
 
 // Not sure if these are really 'Factories', however anytime I was creating more than 1 of an object I just put the code here to simplify
 class Factory {
-    
-    
-    
-    
+
+
+
+
     // Is passed an array of observers to set
     static func createObserver(observer: Any, listeners: observerArray)  {
-        
+
         // Loop through and subscribe to each listener. 'Observer' (normally 'self') is the same for each item in the array passed in.
         // However this can be used for different VC's, models, etc...
-        
+
         // If array is 'empty' it isn't nil (I tested this) so let's log that an empty array was passed.
         if listeners.count > 0 {
             for listener in listeners {
@@ -31,10 +31,10 @@ class Factory {
             print("Listener array not set.")
         }
     }
-    
+
     // During the end of game state, functioning observers were causing issues. So stateEndOfGame just kills them all.
     static func killObserver(observer: Any, listeners: observerArray)  {
-        
+
         // If array is 'empty' it isn't nil (I tested this) so let's log that an empty array was passed.
         if listeners.count > 0 {
             for listener in listeners {
@@ -45,34 +45,34 @@ class Factory {
             print(String(describing: observer))
             print("Listener not removed.")
         }
-        
+
     }
-    
+
     /********* Timer factory *********/
-    
+
     // Superceded by code simplificaiton
-    
+
     /************** Alert Factory *********************/
-    
-    
+
+
     // With overloads and default values, I essentially have 4 functions here
     // Alerts that have more extensive action (handler isn't nil) don't use these (PreferencesVC)
-    
+
     // Used to pass error messages
     static func displayAlert(target: AnyObject, error: Error, title: String = "Firestore Error") {
-        
+
         let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
         target.present(alert, animated: true, completion: nil)
     }
-    
-    
+
+
     // Used to pass strings
     static func displayAlert(target: AnyObject, message: String, title: String = "Game Error") {
-        
+
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
         target.present(alert, animated: true, completion: nil)
     }
-    
+
 }
